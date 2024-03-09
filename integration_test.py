191d649d8645d620e21c1a293f63ea466def51b5
satret@niuitmo.ru <satret@niuitmo.ru>
2024-03-09 13:06:06 +0300

    fix

--- integration_test.py
+++ integration_test.py
@@ -1,29 +1,16 @@
+import pytest
 import yaml
-from pathlib import Path
-from machine.simulation import simulation
-from machine.translator import parse_code
+from simulation import simulation
+from translator import parse_code
 
-def read_asm_file(filename):
-    with open(filename, 'r') as file:
-        return file.readlines()
-
-def read_golden_file(filename):
-    with open(filename, 'r') as file:
-        return yaml.safe_load(file)
-
-def assemble_and_run(asm_file):
-    # Считываем исходный ассемблерный код
-    asm_code = read_asm_file(asm_file)
-
-    # Транслируем ассемблерный код в машинный код
+# Функция для сборки и запуска ассемблерной программы
+def assemble_and_run(asm_file_path):
+    with open(asm_file_path, 'r') as file:
+        asm_code = file.read()
     data_memory, instructions = parse_code(asm_code)
-
-    # Запускаем симуляцию. Предполагаем, что ввод для машины пустой и размер памяти - 256
-    # Вы можете изменить эти значения в зависимости от ваших нужд
-    output = simulation({'data': data_memory, 'code': instructions}, input_tokens=[], data_memory_size=256, simulation_limit=1000)
-
+    # Предполагаем, что нет входных данных для программы, и ограничение на количество инструкций достаточно велико
+    output = simulation({'data': data_memory, 'code': instructions}, [], 256, 10000)
     return output
-
 
 @pytest.mark.parametrize("test_case", ["cat", "hello", "prob2"])
 def test_assembly_examples(test_case):
